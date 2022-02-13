622d22df367885c831eb0dd021c5a008f3b152f6
bidika6 <93795173+bidika6@users.noreply.github.com>
2022-02-13 17:09:50 -0500

    new

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
-import {GlobalStyle} from "./globalStyles";
-import Hero from "./components/Hero";
-import Products from "./components/Products";
-import {productData} from "./components/Products/data";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-      <Router>
-          <GlobalStyle />
-
-          <Hero />
-          <Products heading='Select your food item' data ={productData} />
-      </Router>
-
-  );
+    return (
+        <Router>
+            <GlobalStyle />
+            <Hero />
+            <Products heading='Choose your favorite' data={productData} />
+            <Feature />
+            <Products heading='Treat yourself to our delicious sides' data={productDataTwo} />
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
-
 
--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,6 +1,9 @@
 import product1 from "../../images/prod1RESTv1.jpg";
 import product2 from "../../images/prod2RESTv1.jpg"
 import product3 from "../../images/product3RESTv1.jpg";
+import sweet1 from '../../images/lassi.jpg';
+import sweet2 from '../../images/gulab.jpg';
+import sweet3 from '../../images/Kheer.jpg';
 
 export const productData = [
     {
@@ -36,4 +39,35 @@ export const productData = [
 
 
 
-]
+];
+
+export const productDataTwo = [
+    {
+        img: sweet2,
+        alt: 'Gulab Jamun',
+        name: 'Gulab Jamun',
+        desc:
+            'Fried milk balls soaked in sweet syrup, such as rose syrup or honey',
+        price: '$6.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: sweet3,
+        alt: 'Kheer',
+        name: 'Kheer',
+        desc:
+            'Delicious rice pudding with taste inspired by Natraj',
+        price: '$8.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: sweet1,
+        alt: 'Lassi',
+        name: 'Lassi',
+        desc:
+            'A declious combination of yogurt, milk and nuts, with a hint of mango',
+        price: '$7.99',
+        button: 'Add to Cart'
+    }
+];
+
